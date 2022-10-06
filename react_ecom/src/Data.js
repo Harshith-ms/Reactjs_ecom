514be7198acb683f298caa0bac411bf5049a1d9f
Harshith MS <80873813+Harshith-ms@users.noreply.github.com>
2022-10-06 10:52:06 +0530

    Add files via upload

---
+++ react_ecom/src/Data.js
@@ -0,0 +1,44 @@
+export default {
+  users: [
+    { username: "regular", accessLevel: 1, password: "password" },
+    { username: "admin", accessLevel: 0, password: "password" }
+  ],
+  initProducts: [
+    {
+      name: "Puma Shoes",
+      image: "https://i.pinimg.com/736x/bf/54/64/bf54643f23bc867238e41c68101744c2.jpg",
+      stock: 10,
+      price: 3000,
+      shortDesc: "This is a genuine product of Puma Sports India Pvt Ltd. The product comes with a standard brand warranty of 90 days.",
+      description:
+        "Manufacturing defects: stitching pasting issues, mesh quality, sole integrity. Exchange available within 90 days of purchase, provided the product is not inappropriately used as determined by the Brand"
+    },
+    {
+      name: "bags",
+      image:"https://images-eu.ssl-images-amazon.com/images/I/41zXlQ10r1L._AC_SR400,600_.jpg",
+      stock: 20,
+      price: 2000,
+      shortDesc: "A small bag that can be carried with a single hand is sometimes called a handbag, purse, or pocketbook.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "shirts",
+      image:"https://m.media-amazon.com/images/I/81RnMaaSuRL._UL1500_.jpg",
+      stock: 15,
+      price: 1000,
+      shortDesc: "Experience comfort like never before with the Ramraj cotton shirt fabrics. Superior quality linen white shirt material at the best price.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "shorts",
+      image:"https://assets.ajio.com/medias/sys_master/root/20220819/J8Ir/62fe857df997dd394c02d828/-473Wx593H-461152806-white-MODEL.jpg",
+      stock: 5,
+      price: 500,
+      shortDesc: "There are a variety of shorts, ranging from knee-length short trousers that can in some situations be worn as formal clothes to beachwear and athletic shorts.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    }
+  ]
+};
